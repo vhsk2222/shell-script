d1d23276f376e565ab7db2274121330b1d6f0aaa
vhsk222 <vhsandeepkumar@gmail.com>
2024-09-09 13:39:31 +0530

    Added 5th script in push

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,7 +3,7 @@
 
 echo " Please enter your username"
 
-read USERNAME # the value entered above will be automatically attached to usernma variable 
+read -s USERNAME # the value entered above will be automatically attached to usernma variable 
 
 
 echo "Username is $USERNAME "
