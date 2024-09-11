0da4249a2319ecfab4ca5fb499c759f91f523694
vhsk222 <vhsandeepkumar@gmail.com>
2024-09-12 02:46:10 +0530

    Conditons part 1

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,7 +2,9 @@
 
  NUMBER=$1
 
- if [ $NUMBER -gt 100 ];
- then echo "Given $NUMBER is greater than 100"
- else echo "Give $NUMBER is less than 100"
+ if [ $NUMBER -gt 100 ]
+ then 
+    echo "Given $NUMBER is greater than 100"
+ else 
+    echo "Given $NUMBER is less than 100"
  fi
