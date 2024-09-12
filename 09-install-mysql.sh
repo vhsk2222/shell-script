4e8a3a3661f33e751063586faac03ee9f43ce73c
vhsk222 <vhsandeepkumar@gmail.com>
2024-09-12 07:47:25 +0530

    Conditons part 2 MYSQL INSTALL

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -3,9 +3,15 @@
 ID=$(id -u)
 if [ $ID -ne 0 ]
 then echo "ERROR: A ROOT USER"
-exit 1 #You can give other than zero, that will stop the programm if the pervious commans is a failure 
-echo "NOT FORWRDING"
+exit 1 #You can give other than zero, that will stop the programm if the pervious commans is a failure
 else echo "You are ROOT"
 fi  #reverse of if indiating condition end
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then echo "INSTALLATION FAILED"
+exit 1
+else echo "INSTALLATION SUCCESSFUL"
+fi 
+
