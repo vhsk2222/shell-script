e68cfcc1f65758bd1c515f51f82b7bede3a0416c
vhsk222 <vhsandeepkumar@gmail.com>
2024-09-12 09:08:08 +0530

    Conditons part 2 MYSQL INSTALL

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,19 +2,22 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 echo "Script Name: $0"
 LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
 if [ $1 -ne 0 ]
-then echo "ERROR: $2 FAILED"
+then echo -e "ERROR: $2 $R FAILED $N"
 exit 1
-else echo "$2 .... SUCCESS"
+else echo -e "$2 .... $G SUCCESS $N"
 fi
 }
 
 if [ $ID -ne 0 ]
-then echo "ERROR: NOT A ROOT USER"
+then echo -e "$R ERROR: NOT A ROOT USER $N"
 exit 1 #You can give other than zero, that will stop the programm if the pervious commans is a failure
 else echo "You are ROOT"
 fi 
