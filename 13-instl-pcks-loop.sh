a539a189c2601596049ea96cc851453808780d0d
vhsk222 <vhsandeepkumar@gmail.com>
2024-09-12 11:27:20 +0530

    Conditons part 2 MYSQL INSTALL

--- 13-instl-pcks-loop.sh
+++ 13-instl-pcks-loop.sh
@@ -29,7 +29,7 @@ fi
 for package in $@
 do
     yum list installed $package &>> LOGFILE
-    if [ $? -ne 0]
+    if [ $? -ne 0 ]
     then
         yum install $package -y &>> LOGFILE
         VALIDATE $? "Installation of $package"
