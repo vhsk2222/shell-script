e755d1c02de53a1aa865b0a06b46be3b8f97cfd3
vhsk222 <vhsandeepkumar@gmail.com>
2024-09-12 11:24:18 +0530

    Conditons part 2 MYSQL INSTALL

--- 13-instl-pcks-loop.sh
+++ 13-instl-pcks-loop.sh
@@ -10,7 +10,8 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "Sscript started executing $TIMESTAMP" &>> LOGFILE
 
-VALIDATE(){if [ $1 -ne 0] 
+VALIDATE(){
+if [ $1 -ne 0] 
 then echo -e "$2 ...$R Failed ..$N"
 else echo -e "$2 ...$G SUCCESSSSS ..$N"
 }
