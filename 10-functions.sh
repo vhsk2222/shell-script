fdf5c9766e57379e4a5bdf85afcf606e68b88cec
vhsk222 <vhsandeepkumar@gmail.com>
2024-09-12 08:30:23 +0530

    Conditons part 2 MYSQL INSTALL

--- 10-functions.sh
+++ 10-functions.sh
@@ -11,7 +11,7 @@ fi
 }
 
 if [ $ID -ne 0 ]
-then echo "ERROR: A ROOT USER"
+then echo "ERROR: NOT A ROOT USER"
 exit 1 #You can give other than zero, that will stop the programm if the pervious commans is a failure
 else echo "You are ROOT"
 fi 
