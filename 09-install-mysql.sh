ad5be60e7e2f648889709d7c1f529323546d3909
vhsk222 <vhsandeepkumar@gmail.com>
2024-09-12 07:44:04 +0530

    Conditons part 2 MYSQL INSTALL

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,6 +4,7 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then echo "ERROR: A ROOT USER"
 exit 1 #You can give other than zero, that will stop the programm if the pervious commans is a failure 
+echo "NOT FORWRDING"
 else echo "You are ROOT"
 fi  #reverse of if indiating condition end
 
